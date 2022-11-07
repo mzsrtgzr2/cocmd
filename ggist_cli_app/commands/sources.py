227c53179d10f1641d8ce4d92d5abd8469f22822
Moshe Roth <mzsrtgzr2@gmail.com>
2022-11-07 13:22:27 +0000

    wip

--- ggist_cli_app/commands/sources.py
+++ ggist_cli_app/commands/sources.py
@@ -2,7 +2,7 @@ import click
 from ggist_cli_app.context import click_pass_context
 from ggist_cli_app.core.aliases import recreate_aliases
 from ggist_cli_app.core.source import Source
-from ggist_cli_app.utils.fs import file_read_lines, file_write_lines
+from ggist_cli_app.utils.fs import file_write_lines
 
 @click.command()
 @click.argument('source')
