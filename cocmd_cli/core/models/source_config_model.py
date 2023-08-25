dd7ecf49f75ecb1a525cc8e7c34ff46015485b22
morot <morot@morots-MacBook-Pro.local>
2023-08-25 19:57:33 +0300

    fixes automation

--- cocmd_cli/core/models/source_config_model.py
+++ cocmd_cli/core/models/source_config_model.py
@@ -1,12 +1,12 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from cocmd_cli.utils.io import DictLoader, YamlIO, normalize_path
 from typing import List, Optional
 from .script_model import ScriptModel
 
 
-@dataclass(frozen=True)
+@dataclass
 class Automation:
-    subcommand: str
+    name: str
     file: Optional[str]
     content: Optional[ScriptModel]
 
@@ -21,6 +21,6 @@ class Automation:
 @dataclass(frozen=True)
 class SourceConfigModel(DictLoader):
     name: str
-    aliases: str
-    paths: List[str]
-    automations: List[Automation]
+    aliases: Optional[str] = field(default_factory=str)
+    paths: Optional[List[str]] = field(default_factory=list)
+    automations: Optional[List[Automation]] = field(default_factory=list)
--- cocmd_cli/core/os.py
+++ cocmd_cli/core/os.py
@@ -1,13 +1,12 @@
 from enum import Enum
 
-OS_OVERRIDES = {
-    'darwin': 'osx'
-}
+OS_OVERRIDES = {"darwin": "osx"}
+
 
 class OS(Enum):
-    ANY='any'
-    OSX='osx'
-    LINUX='linux'
+    ANY = "any"
+    OSX = "osx"
+    DEBIAN = "debian"
 
     def __str__(self):
         return self.value
@@ -21,5 +20,5 @@ class OS(Enum):
 
         if text in OS_OVERRIDES:
             return OS.from_str(OS_OVERRIDES[text])
-    
+
         raise ValueError(f"unable to find os {text}")
