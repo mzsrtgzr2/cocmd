1f1be2a76ed80b610df39f970301ebc1f9874c6e
morot <morot@morots-MacBook-Pro.local>
2023-08-25 23:02:11 +0300

    generate-docs

--- website/scripts/generate-docs.py
+++ website/scripts/generate-docs.py
@@ -5,13 +5,26 @@ from cocmd_cli.settings import Settings
 
 from cocmd_cli.utils.repository import find_cocmd_files
 
-MD_TECH = """
----
+MD_TECH = """---
 sidebar_position: {position}
 ---
 
 # {name}
 
+## aliases
+
+```bash
+{aliases}
+```
+
+## paths
+
+{paths}
+
+
+### automations
+
+{automations}
 """
 
 if __name__ == "__main__":
@@ -23,5 +36,22 @@ if __name__ == "__main__":
     for ii, loc in enumerate(locations):
         source = Source(loc, settings)
 
-        with open(f"./website/docs/technologies/{source.name}", "w") as fp:
-            fp.write(MD_TECH.format(name=source.name, position=ii + 1))
+        paths = "\n".join(map(lambda s: f"- {s}", source.paths))
+
+        automations = "\n".join(
+            map(
+                lambda a: f"- `{source.name}.{a.name}` - {a.content.description}",
+                source.automations,
+            )
+        )
+
+        with open(f"./website/docs/technologies/{source.name}.md", "w") as fp:
+            fp.write(
+                MD_TECH.format(
+                    name=source.name.capitalize(),
+                    position=ii + 1,
+                    aliases=source.aliases,
+                    paths=paths,
+                    automations=automations,
+                ),
+            )
