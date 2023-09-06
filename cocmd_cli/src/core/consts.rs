80cf27d6b37d5d8c2e7f40235584e4c56893cbb1
Your Name <mosher@mosher.com>
2023-09-06 22:07:14 +0300

    home dir calc lazy

--- cocmd_cli/src/core/consts.rs
+++ cocmd_cli/src/core/consts.rs
@@ -1,7 +1,14 @@
 #![allow(dead_code)]
+use dirs;
+use lazy_static::lazy_static;
 
-pub const HOME: &str = ".cocmd";
-pub const SOURCE_CONFIG_FILE: &str = "cocmd.yaml";
+
+lazy_static! {
+    pub static ref HOME: String = {
+        let home_dir = dirs::home_dir().unwrap();
+        format!("{}/.cocmd", home_dir.to_str().unwrap())
+    };
+}pub const SOURCE_CONFIG_FILE: &str = "cocmd.yaml";
 pub const DEFAULT_TERMINAL: &str = "bash";
 pub const CONFIG_FILE: &str = "config.yaml";
 pub const SOURCES_FILE: &str = "sources.txt";
